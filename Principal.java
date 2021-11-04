abb9928d40a22a48ea817654fd03fa0cd96b496e
carlam97 <carmartins04@hotmail.com>
2021-11-04 19:18:04 +0000

    a

--- Principal.java
+++ Principal.java
@@ -1,10 +1,38 @@
+import java.util.Scanner;
+
+
 public class Principal {
     public static void main(String[] args){
 
+        Scanner sc = new Scanner(System.in);
         Aluno a = new Aluno();
         Professor p = new Professor();
         Pessoa ps = new Pessoa();
 
+        //Código para adicionar as informações
+        
+     /* System.out.println("Digite seu nome: ");
+        a.setNome(sc.nextLine());
+
+        System.out.println("Digite seu cpf: ");
+        a.setCpf(sc.nextLine());
+
+        System.out.println("Digite seu endereco: ");
+        a.setEndereco(sc.nextLine());
+
+        System.out.println("Digite seu telefone: ");
+        a.setTelefone(sc.nextLine());
+
+        System.out.println("Digite seu curso: ");
+        a.setCurso(sc.nextLine());
+
+        System.out.println("Digite sua nota da P1: ");
+        a.setNota1(sc.nextLine());
+
+        System.out.println("Digite sua nota da P2: ");
+        a.setNota2(sc.nextLine());
+          
+        */
         //Dados do aluno
         a.setNome("Carla Martins");
         a.setEndereco("Rua das Flores");
